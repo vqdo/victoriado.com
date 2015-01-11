d2e53dfe9a209dcfbcbccf6c638ed82a537d5271
Victoria Do <victoriado@outlook.com>
2015-01-11 12:57:05 -0800

    Basic works layout

--- js/master.js
+++ js/master.js
@@ -2,15 +2,6 @@ var home = (function() {
 	var self = {};
 	var $logo = $('#logo');
 
-	self.init = function() {
-	}
-	// home.scaleToLogoSize = function() {
-	// 	var $logo = $('.brand img');
-	// 	var $banner = $('header');
-	// 	var height = $logo.height();
-	// 	console.log(height);
-	// 	$banner.height(height);
-	// }
 	self.onResize = function() {
 
 	}
@@ -18,20 +9,132 @@ var home = (function() {
 	/** 
 	 * Update opacity of the logo based on scroll distance 
 	 */
-	self.updateOpacity = function(distance) {
-		var interaction_distance = $logo.height();
-		var opacity = distance/interaction_distance;
-		opacity = Math.max(1 - Math.min(1, opacity), 0.1);
+	self.logoController = {
+		init: function() { },
+		onScrollDistance: function(distance) {	
+			var interaction_distance = $logo.height();
+			var opacity = distance/interaction_distance;
+			opacity = Math.max(1 - Math.min(1, opacity), 0.1);
 
-		$logo.css("opacity", opacity);
+			$logo.css("opacity", opacity);
+		}
 	}
+
+	var worksMap = {};
+	self.portfolioController = {
+
+		init: function() {
+			this.attachThumbHandler();
+			this.attachThumbExpand();
+			this.constructColumns($('.portfolio-gallery'), $('.portfolio-thumb'));
+			this.makeWorksMap();
+		},
+
+
+		makeWorksMap: function() {
+			var thumbnails = $('.portfolio-thumb');
+			var links = $('.works-links li');
+
+			if(thumbnails.length != links.length) {
+				console.warn("Number of links does not match number of thumbnails.");
+			}
+
+			// returns 'name' in string formatted 'x-name'
+			var extractName = function(classNames) {
+				var parts = classNames.split('-');
+				if(parts.length > 1) {
+					return parts[1];
+				}
+			
+				console.error("Invalid format: " + classNames);
+				return null;
+			}
+
+			$.each(thumbnails, function(i, thumbnail) {
+				thumbnail = $(thumbnail);
+				var name = extractName(thumbnail.attr('id'));
+				worksMap[name] = worksMap[name] || {};
+				worksMap[name].thumbnail = thumbnail;
+			});
+			$.each(links, function(i, link) {
+				link = $(link);
+				var name = extractName(link.attr('id'));
+				worksMap[name] = worksMap[name] || {};
+				worksMap[name].link = link;
+			});
+		},
+
+		attachThumbExpand: function() {
+			var toggleMoreInfo = function($thumbnail) {
+				$thumbnail.find('.more-info').toggle();
+				$thumbnail.toggleClass('expanded');
+			}
+
+			$('.see-more-button').click(function(evt) {
+				evt.preventDefault();
+				var $thumbnail = $(this).parent();
+				toggleMoreInfo($thumbnail);
+
+				$thumbnail.find('.see-more').remove();
+			});
+		},
+
+		attachThumbHandler: function() {
+			var createSeeMore = function() {
+				return $('<div class="see-more">Click to see more</div>');
+			}
+
+			var $works = $('.portfolio-thumb');
+
+			$works.mouseover(function(evt) {;
+				evt.stopPropagation();
+
+				var $this = $(this);
+				if(!$this.has('.see-more').length && !$this.is('.expanded')) {
+					$this.append(createSeeMore());
+				}
+			}).mouseleave(function(evt) {;
+				// console.log($(evt.target).find('.see-more'));
+				//console.log(evt.target);
+				$(this).find('.see-more').remove();
+			});
+		},
+
+		// Create columns for the portfolio works to simulate the "masonry" style
+		constructColumns: function($container, $items) {
+
+			var itemWidth = $items.outerWidth();
+			var maxWidth = $container.width();
+			var n = Math.floor(maxWidth/(itemWidth + 10)); // number of cols
+
+			var makeColumn = function(i) {
+				return $('<div />')
+					.width(itemWidth)
+					.addClass('fl col col' + i);
+			}
+
+			var columns = [];
+			for(var i = 0; i < n; i++) {
+				var col = makeColumn(i);
+				$container.append(col);
+				columns.push(col);
+			}
+
+			$.each($items, function(i, item) {
+				columns[i % n].append($(item).detach());
+			});
+		}		
+	}
+
 
 	return self;
 })();
 
 $(document).ready(function() {
-	home.init();
+	home.logoController.init();
+	home.portfolioController.init();
+
 	$(window).scroll(function() {
-		home.updateOpacity($(window).scrollTop());
-	});
+		home.logoController.onScrollDistance($(window).scrollTop());
+	});		
 });
